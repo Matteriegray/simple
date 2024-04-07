88eb180ed535e80491c3714734fca6f0ec8e9869
Matteriegray <codevista208@gmail.com>
2024-04-07 18:31:06 +0530

    Intial Commit

---
+++ palindrome.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main ()
+{
+long num, temp, reverse;
+int rem;
+printf("Input a number\n");
+scanf("%ld", &num);
+temp = num;
+reverse=0;
+while (num>0)
+{
+rem = num % 10;
+reverse = reverse * 10 + rem;
+num = num / 10;
+}
+if (temp == reverse)
+{
+printf("%ld is a palindrome", temp);
+}
+else
+{
+printf("%ld is not a palindrome", temp);
+}
+}
